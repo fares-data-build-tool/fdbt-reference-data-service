ec6a9d9c8232843123270b5a1f1ef310e744c789
Danny Davies <danny.davies@infinityworks.com>
2020-03-31 09:13:59 +0100

    Created connect_to_database function to split out functionality

--- src/uploaders/xml-uploader/handler.py
+++ src/uploaders/xml-uploader/handler.py
@@ -16,6 +16,28 @@ logger.setLevel(logging.INFO)
 s3 = boto3.client('s3')
 ssm = boto3.client('ssm')
 
+def connect_to_database():
+    rds_host = os.getenv('RDS_HOST')
+    db_name = 'fdbt'
+    username = ssm.get_parameter(
+        Name='fdbt-rds-reference-data-username',
+        WithDecryption=True
+    )
+    password = ssm.get_parameter(
+        Name='fdbt-rds-reference-data-password',
+        WithDecryption=True
+    )
+    try:
+        connection = pymysql.connect(
+            rds_host, user=username, passwd=password, db=db_name, connect_timeout=5)
+        return connection
+    except pymysql.MySQLError as e:
+        logger.error(
+            "ERROR: Unexpected error: Could not connect to MySQL instance.")
+        logger.error(e)
+        sys.exit()
+
+    logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
 
 def handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
@@ -28,7 +50,8 @@ def handler(event, context):
         tree = ET.parse(file_dir)
         xml_data = tree.getroot()
         xml_string = ET.tostring(xml_data, encoding='utf-8', method='xml')
-        data_dict = xmltodict.parse(xml_string, process_namespaces=True, namespaces=xmltodict_namespaces)
+        data_dict = xmltodict.parse(
+            xml_string, process_namespaces=True, namespaces=xmltodict_namespaces)
         print(json.dumps(data_dict, indent=4))
 
         # DATA PROCESSING FUNCTIONS HERE
@@ -41,32 +64,22 @@ def handler(event, context):
 
 
 def insert_into_database(key, bucket):
-    rds_host = os.getenv('RDS_HOST')
-    db_name = 'fdbt'
-    username = ssm.get_parameter(
-        Name='fdbt-rds-reference-data-username',
-        WithDecryption=True
-    )
-    password = ssm.get_parameter(
-        Name='fdbt-rds-reference-data-password',
-        WithDecryption=True
-    )
+    query_array = [None]
+    table_name = ''
+    item_count = 0
 
-    try:
-        connection = pymysql.connect(
-            rds_host, user=username, passwd=password, db=db_name, connect_timeout=5)
-        query_array = [None]
+    connection = connect_to_database()
 
-        for query_line in query_array:
-            with connection.cursor() as cursor:
-                cursor.execute(query_line)
+    for query_line in query_array:
+        with connection.cursor() as cursor:
+            cursor.execute(query_line)
+            connection.commit()
+            cursor.execute(f"select * from {table_name}")
+            for row in cursor:
+                item_count += 1
+                logger.info(row)
+                print(row)
 
-        connection.commit()
-
-        logger.info("SUCCESS: Data insertion to RDS MySQL instance succeeded")
-
-    except pymysql.MySQLError as e:
-        logger.error(
-            "ERROR: Unexpected error: Could not connect to MySQL instance.")
-        logger.error(e)
-        sys.exit()
+    connection.commit()
+    logger.info("SUCCESS: Data insertion to RDS MySQL instance succeeded")
+    return "Added %d items from RDS MySQL table" %(item_count)
